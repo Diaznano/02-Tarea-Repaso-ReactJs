600a6221818f5bc7998910049ae2508c54c52c0c
Nicolas Larrosa <nicolaslarrosa.mail@gmail.com>
2023-08-02 00:08:07 -0300

    Template Inicial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+
+
+function App() {
+
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col"></div>
+          <div class="col"></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
